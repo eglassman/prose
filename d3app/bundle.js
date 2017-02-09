649677b663cdd0ee260ff239cc9292aadc9cff08
Elena Glassman <elena.glassman@gmail.com>
2017-02-09 02:07:55 -0800

    now i can use array-counter on the dom elements!

--- d3app/bundle.js
+++ d3app/bundle.js
@@ -1,16 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var unique = require('uniq');
-var arrayCounter = require('array-counter');
-
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
-
-console.log(unique(data));
-
-var arr = ["foo", "bar", 10, 40, "foo", 10, "Hello World"];
-
-console.log(arrayCounter(arr));
-
-},{"array-counter":2,"uniq":3}],2:[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"array-counter":[function(require,module,exports){
 'use strict';
 
 function isArray(obj) {
@@ -35,63 +23,4 @@ module.exports = function (arr) {
   return obj;
 };
 
-},{}],3:[function(require,module,exports){
-"use strict"
-
-function unique_pred(list, compare) {
-  var ptr = 1
-    , len = list.length
-    , a=list[0], b=list[0]
-  for(var i=1; i<len; ++i) {
-    b = a
-    a = list[i]
-    if(compare(a, b)) {
-      if(i === ptr) {
-        ptr++
-        continue
-      }
-      list[ptr++] = a
-    }
-  }
-  list.length = ptr
-  return list
-}
-
-function unique_eq(list) {
-  var ptr = 1
-    , len = list.length
-    , a=list[0], b = list[0]
-  for(var i=1; i<len; ++i, b=a) {
-    b = a
-    a = list[i]
-    if(a !== b) {
-      if(i === ptr) {
-        ptr++
-        continue
-      }
-      list[ptr++] = a
-    }
-  }
-  list.length = ptr
-  return list
-}
-
-function unique(list, compare, sorted) {
-  if(list.length === 0) {
-    return list
-  }
-  if(compare) {
-    if(!sorted) {
-      list.sort(compare)
-    }
-    return unique_pred(list, compare)
-  }
-  if(!sorted) {
-    list.sort()
-  }
-  return unique_eq(list)
-}
-
-module.exports = unique
-
-},{}]},{},[1]);
+},{}]},{},[]);
--- d3app/js/main.js
+++ d3app/js/main.js
@@ -6,5 +6,18 @@ var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 console.log(unique(data));
 
 var arr = ["foo", "bar", 10, 40, "foo", 10, "Hello World"];
+//var doctype_arr = $('.doctype.compiled').map(function (i,e) {return $(e).text();}).toArray();
+var new_arr = [];
+$('.doctype.compiled').each(function(i,e){
+	console.log(i,e);
+	new_arr.push($(e).text());
+});
+console.log(new_arr)
+//var doctype_arr = $('.doctype.compiled').toArray().map(function (i,e){return $(i).text()}); //$('.doctype.compiled').toArray();
+//console.log('doctype_arr',doctype_arr)
+//var isArrayBool = Array.isArray(doctype_arr);
 
 console.log(arrayCounter(arr));
+//console.log(isArrayBool);
+//console.log(doctype_arr);
+console.log(arrayCounter(new_arr));
