3d1437a6c30fd90e35018ae3d9c5356605aa0068
Elena Glassman <elena.glassman@gmail.com>
2017-02-08 23:35:02 -0800

    got a js counter, whoo!

--- d3app/bundle.js
+++ d3app/bundle.js
@@ -1,13 +1,41 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var unique = require('uniq');
+var arrayCounter = require('array-counter');
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
 console.log(unique(data));
 
-// var counter = require( 'babel-polyfill' ) ;
-// console.log(counter)
-},{"uniq":2}],2:[function(require,module,exports){
+var arr = ["foo", "bar", 10, 40, "foo", 10, "Hello World"];
+
+console.log(arrayCounter(arr));
+
+},{"array-counter":2,"uniq":3}],2:[function(require,module,exports){
+'use strict';
+
+function isArray(obj) {
+  return Object.prototype.toString.call(obj) === "[object Array]";
+}
+
+module.exports = function (arr) {
+  if(!isArray(arr)) {
+    throw new TypeError('Expected an array.');
+  }
+
+  var obj = {};
+
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i] in obj) {
+      obj[arr[i]]++;
+    }else {
+      obj[arr[i]] = 1;
+    }
+  }
+
+  return obj;
+};
+
+},{}],3:[function(require,module,exports){
 "use strict"
 
 function unique_pred(list, compare) {
--- d3app/js/main.js
+++ d3app/js/main.js
@@ -1,8 +1,10 @@
 var unique = require('uniq');
+var arrayCounter = require('array-counter');
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
 console.log(unique(data));
 
-// var counter = require( 'babel-polyfill' ) ;
-// console.log(counter)
+var arr = ["foo", "bar", 10, 40, "foo", 10, "Hello World"];
+
+console.log(arrayCounter(arr));
