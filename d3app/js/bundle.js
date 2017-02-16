f4b95d6d686c47d783fc8bb632bf9df3f4169491
Elena Glassman <elena.glassman@gmail.com>
2017-02-16 12:44:17 -0800

    adding files for sharing with undergrads

--- d3app/js/bundle.js
+++ d3app/js/bundle.js
@@ -23,23 +23,130 @@ $.getJSON("data/html_vs_js_joined.json", function(data){
         $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-include"])+'</span></code></pre></div>');
     });
 
-    $('body').append('<h3>js-d3</h3>');
+    $('body').append('<h3>js-canvas</h3>');
     data.forEach(function(s){
         //console.log(s);
-        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-d3"])+'</span></code></pre></div>');
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-canvas"])+'</span></code></pre></div>');
     });
 
-    // $('body').append('<h3>js-d3-tokenized</h3>');
-    // data.forEach(function(s){
-    //     //console.log(s);
-    //     $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+JSON.stringify(esprima.tokenize(s["js-d3"]), null, 4)+'</span></code></pre></div>');
-    // });
-
-    $('body').append('<h3>js-d3-parsed</h3>');
+    $('body').append('<h3>js-time-formatting</h3>');
     data.forEach(function(s){
         //console.log(s);
-        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+JSON.stringify(esprima.parse(s["js-d3"]), null, 4)+'</span></code></pre></div>');
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-time-formatting"])+'</span></code></pre></div>');
     });
+
+    $('body').append('<h3>js-vardec</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-vardec"])+'</span></code></pre></div>');
+    });
+
+    
+    $('body').append('<h3>js-core-selections-and-vardec</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-core-selections-and-vardec"])+'</span></code></pre></div>');
+    });
+
+    $('body').append('<h3>js-time-scales</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-time-scales"])+'</span></code></pre></div>');
+    });
+
+    $('body').append('<h3>js-scale-quant</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-scale-quant"])+'</span></code></pre></div>');
+    });
+    
+    $('body').append('<h3>js-scale-ordinal</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-scale-ordinal"])+'</span></code></pre></div>');
+    });
+
+    
+    $('body').append('<h3>js-svg-axis</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-svg-axis"])+'</span></code></pre></div>');
+    });
+
+    
+    $('body').append('<h3>js-layout-stack</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-layout-stack"])+'</span></code></pre></div>');
+    });
+
+    
+    $('body').append('<h3>js-core-arrays</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-core-arrays"])+'</span></code></pre></div>');
+    });
+
+    
+    $('body').append('<h3>js-svg-shapes</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-svg-shapes"])+'</span></code></pre></div>');
+    });
+
+    $('body').append('<h3>js-arc</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-arc"])+'</span></code></pre></div>');
+    });
+
+    
+    $('body').append('<h3>js-core-selections</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-core-selections"])+'</span></code></pre></div>');
+    });
+
+    $('body').append('<h3>js-pie</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-pie"])+'</span></code></pre></div>');
+    });
+
+    $('body').append('<h3>js-context</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-context"])+'</span></code></pre></div>');
+    });
+
+    $('body').append('<h3>js-request-call</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-request-call"])+'</span></code></pre></div>');
+    });
+
+    $('body').append('<h3>js-request-data</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-request-data"])+'</span></code></pre></div>');
+    });
+
+    $('body').append('<h3>js-request-auxfcn</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-request-auxfcn"])+'</span></code></pre></div>');
+    }); 
+
+    $('body').append('<h3>js-request-callback</h3>');
+    data.forEach(function(s){
+        //console.log(s);
+        $('body').append('<div><pre style="margin-top: 0px; margin-bottom: 0px;"><code><span class="'+s["source"]+'">'+escape(s["js-request-callback"])+'</span></code></pre></div>');
+    });    
+
+    
+
+   
+
 });
 
 },{"escape-html":2,"esprima":3}],2:[function(require,module,exports){
